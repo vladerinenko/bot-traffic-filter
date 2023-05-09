8cc01a882e931283b08140ab4830163f930cef93
vladerinenko <vlad.erinenko@gmail.com>
2023-05-09 11:28:12 +0300

    add .env

--- db.php
+++ db.php
@@ -1,10 +1,12 @@
 <?php
-$servername = "127.0.0.1:3306";
-$username = "root";
-$password = "root";
-$dbname = "traffic";
+$env = parse_ini_file('.env');
 
-$db = new mysqli($servername, $username, $password, $dbname);
+$db_host = $env["DB_HOST"];
+$username = $env["DB_USERNAME"];
+$password = $env["DB_PASSWORD"];
+$dbname = $env["DB_NAME"];
+
+$db = new mysqli($db_host, $username, $password, $dbname);
 if ($db->connect_error) {
     die("Connection failed: " . $db->connect_error);
 }
